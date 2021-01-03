b5ccec38a9a5f72bbfbba0b56f52dd1cf607c2a2
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2021-01-03 18:53:04 +0100

    modification du temps de validation du token

--- src/main/java/com/bibliotheque/controller/UserController.java
+++ src/main/java/com/bibliotheque/controller/UserController.java
@@ -79,7 +79,7 @@ public class UserController {
                                 .map(GrantedAuthority::getAuthority)
                                 .collect(Collectors.toList()))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 600000000))
+                .setExpiration(new Date(System.currentTimeMillis() + 6000000))
                 .signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
         return "Bearer " + token;
     }
